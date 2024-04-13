0c754f1de199970c612ead51a89ca4af0e340437
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2024-04-13 13:01:06 +0300

    AppUserAdmin update

--- the_foodhub/the_foodhub/accounts/admin.py
+++ the_foodhub/the_foodhub/accounts/admin.py
@@ -7,3 +7,9 @@ UserModel = get_user_model()
 @admin.register(UserModel)
 class AppUserAdmin(auth_admin.UserAdmin):
     model = UserModel
+
+    list_display = ('email', 'first_name', 'last_name', 'username', 'role', 'is_active')
+    ordering = ('-date_joined',)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
