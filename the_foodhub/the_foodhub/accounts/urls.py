739e0fb1e49032a4706ab2db4bfd50cf782b5f26
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2024-04-15 20:28:53 +0300

    base template creation

--- the_foodhub/the_foodhub/accounts/urls.py
+++ the_foodhub/the_foodhub/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from the_foodhub.accounts.views import SignInUserView
+from the_foodhub.accounts.views import SignInUserView,  SignUpUserView, signout_user
 
 urlpatterns = (
     path("signup/", SignUpUserView.as_view(), name="signup user"),
+    path("signin/", SignInUserView.as_view(), name="signin user"),
+    path("signout/", signout_user, name="signout user"),
 )
