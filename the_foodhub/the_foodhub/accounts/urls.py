8edbd16e8530f88ff41bfebbffb31731919b80a7
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2024-04-15 20:19:20 +0300

    Creation SignInUserView, SignUpUserView, signout_user view

--- the_foodhub/the_foodhub/accounts/urls.py
+++ the_foodhub/the_foodhub/accounts/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = ()
+from django.urls import path
+from the_foodhub.accounts.views import SignInUserView
+
+urlpatterns = (
+    path("signup/", SignUpUserView.as_view(), name="signup user"),
+)
--- the_foodhub/the_foodhub/accounts/views.py
+++ the_foodhub/the_foodhub/accounts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import views as auth_views, logout
+from django.urls import reverse_lazy, reverse
+from django.views import generic as views
 
-# Create your views here.
+
+class SignInUserView(auth_views.LoginView):
+    # template_name = 'accounts/signin_user.html'
+    # redirect_authenticated_user = True
+    # success_url = reverse_lazy('index')
+    pass
+
+
+class SignUpUserView(views.CreateView):
+    # template_name = 'accounts/signup_user.html'
+    # form_class = SpeachCenterUserCreationForm
+    # queryset = Profile.objects.all()
+    #
+    # def get_success_url(self):
+    #     return reverse('details profile', kwargs={'pk': self.object.pk})
+    #
+    # def form_valid(self, form):
+    #     result = super().form_valid(form)
+    #     login(self.request, form.instance)
+    #     return result
+    pass
+
+
+def signout_user(request):
+    logout(request)
+    return redirect('index')
