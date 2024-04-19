b83098af54950c89dd74c1330650971211b90f58
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2024-04-19 11:18:54 +0300

    Vendor model creation

--- the_foodhub/the_foodhub/vendor/admin.py
+++ the_foodhub/the_foodhub/vendor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from the_foodhub.vendor.models import Vendor
+
+
+@admin.register(Vendor)
+class ServicesAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_profile', 'vendor_name', 'vendor_license', 'is_approved', 'created_at', 'modified_at')
--- the_foodhub/the_foodhub/vendor/models.py
+++ the_foodhub/the_foodhub/vendor/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+from the_foodhub.accounts.models import FoodHubUser, Profile
+
+
+class Vendor(models.Model):
+    MAX_VENDOR_NAME_LENGTH = 50
+
+    user = models.OneToOneField(
+        FoodHubUser,
+        related_name='user',
+        on_delete=models.CASCADE,
+    )
+
+    user_profile = models.OneToOneField(
+        Profile,
+        related_name='user_profile',
+        on_delete=models.CASCADE,
+    )
+
+    vendor_name = models.CharField(
+        max_length=MAX_VENDOR_NAME_LENGTH,
+    )
+
+    vendor_license = models.ImageField(
+        upload_to='vendor/licence',
+    )
+
+    is_approved = models.BooleanField(
+        default=False,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    modified_at = models.DateTimeField(
+        auto_now=True,
+    )
+
+    def __str__(self):
+        return self.vendor_name
