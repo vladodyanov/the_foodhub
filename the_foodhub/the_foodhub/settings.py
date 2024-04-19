a096ad959fb7f0ce46fd9d305ba768d70e7eb70b
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2024-04-19 10:57:52 +0300

    Vendor app creation

--- the_foodhub/the_foodhub/settings.py
+++ the_foodhub/the_foodhub/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
 
     'the_foodhub.web',
     'the_foodhub.accounts',
+    'the_foodhub.vendor',
 ]
 
 MIDDLEWARE = [
--- the_foodhub/the_foodhub/urls.py
+++ the_foodhub/the_foodhub/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('the_foodhub.web.urls')),
-    path('', include('the_foodhub.accounts.urls')),
+    path('accounts/', include('the_foodhub.accounts.urls')),
+    path('vendor/', include('the_foodhub.accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
