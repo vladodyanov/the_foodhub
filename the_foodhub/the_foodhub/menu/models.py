aa23d3f1714b4774efd5fa11a7a1ab52cb21e269
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2024-06-12 10:44:03 +0300

    Menu Category model creation

--- the_foodhub/the_foodhub/menu/models.py
+++ the_foodhub/the_foodhub/menu/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from the_foodhub.vendor.models import Vendor
+
+
+class Category(models.Model):
+    MAX_LENGHT_CATEGORY_NAME = 50
+    MAX_LENGHT_SLUG = 100
+    MAX_LENGHT_DESCRIPTION = 250
+
+    vendor = models.ForeignKey(
+        Vendor,
+        on_delete=models.CASCADE
+    )
+    category_name = models.CharField(
+        max_length=MAX_LENGHT_CATEGORY_NAME,
+        unique=True
+    )
+    slug = models.SlugField(
+        max_length=MAX_LENGHT_SLUG,
+        unique=True
+    )
+    descriptions = models.TextField(
+        max_length=MAX_LENGHT_DESCRIPTION,
+        blank=True
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return self.category_name
