5cac0b631ae5cb30580ab5e53d4bb801b8e8fee6
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2024-06-12 10:52:13 +0300

    Menu FoodItem Model creation

--- the_foodhub/the_foodhub/menu/models.py
+++ the_foodhub/the_foodhub/menu/models.py
@@ -33,3 +33,46 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category_name
+
+
+class FoodItem(models.Model):
+    MAX_LENGHT_FOOD_TITLE = 50
+    MAX_LENGHT_SLUG = 100
+    MAX_LENGHT_DESCRIPTION = 250
+    MAX_DIGITS_PRICE = 10
+
+    vendor = models.ForeignKey(
+        Vendor,
+        on_delete=models.CASCADE
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+    food_title = models.CharField(
+        max_length=MAX_LENGHT_FOOD_TITLE
+    )
+    slug = models.SlugField(
+        max_length=MAX_LENGHT_SLUG,
+        unique=True
+    )
+    descriptions = models.TextField(
+        max_length=MAX_LENGHT_DESCRIPTION,
+        blank=True
+    )
+    price = models.DecimalField(
+        max_digits=MAX_DIGITS_PRICE,
+        decimal_places=2
+    )
+    image = models.ImageField(
+        upload_to='foodimages'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return self.food_title
