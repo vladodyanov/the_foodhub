fdbb68422b6445eb48e8110ab326d29b3cddd3c5
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2024-06-15 21:27:45 +0530

    Update Menu admin.py

--- the_foodhub/the_foodhub/menu/admin.py
+++ the_foodhub/the_foodhub/menu/admin.py
@@ -4,6 +4,8 @@ from the_foodhub.menu.models import Category, FoodItem
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('category_name',)}
+    list_display = ('category_name', 'vendor', 'updated_at')
+    search_fields = ('category_name','vendor__vendor_name')
 
 
 admin.site.register(Category, CategoryAdmin)
