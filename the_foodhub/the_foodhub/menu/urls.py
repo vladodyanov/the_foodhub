97d79bfdcaad5871101a219cd9f7b505fffaaf7f
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2024-06-17 11:51:47 +0530

    Menu builder update

--- the_foodhub/the_foodhub/menu/urls.py
+++ the_foodhub/the_foodhub/menu/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from the_foodhub.menu.views import menu_builder
 
 urlpatterns = [
-    path('menu-builder', menu_builder, name='menu_builder'),
+    path('menu-builder/', menu_builder, name='menu_builder'),
 ]
